46c31c7a7903cb8af3b977b1f916612a9cdfa939
Software10 <patilaniket04@gmail.com>
2021-11-23 19:47:54 +0530

    added class based views function, created models and template

--- class_based_view/list_view/admin.py
+++ class_based_view/list_view/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import student
 
 # Register your models here.
+@admin.register(student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id','name','email','roll','course']
--- class_based_view/list_view/views.py
+++ class_based_view/list_view/views.py
@@ -1,3 +1,9 @@
+from django.db.models.base import Model
 from django.shortcuts import render
+from .models import student
+from django.views.generic.list import ListView
 
 # Create your views here.
+class StudentListView(ListView):
+    model = student
+
